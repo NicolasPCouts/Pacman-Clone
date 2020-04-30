465551db3581b0f2595844e3fcaa5b7cf2a9561a
NicolasPCouts <nicolaspereiracoutinho@gmail.com>
2020-04-30 01:03:23 -0300

    Drawing Big Snack

--- Snack.cpp
+++ Snack.cpp
@@ -9,15 +9,18 @@ Snack::Snack(SnackType type, sf::Vector2i gridPos)
     if (type == SmallSnack)
     {
         body = sf::RectangleShape(sf::Vector2f(gameManager->tileWidth, gameManager->tileHeight));
-        texture.loadFromFile("Resources/PacManSprites.png", sf::IntRect(226, 240, 6, 6));
-        body.setFillColor(sf::Color::White);
-        body.setTexture(&texture);
-        body.move(sf::Vector2f(gameManager->tileWidth * gridPos.x, gameManager->tileHeight * gridPos.y));
+        texture.loadFromFile("Resources/PacManSprites.png", sf::IntRect(226, 240, 7, 7));
     }
+    else
+    {
+        body = sf::RectangleShape(sf::Vector2f(gameManager->tileWidth, gameManager->tileHeight));
+        texture.loadFromFile("Resources/PacManSprites.png", sf::IntRect(233, 240, 8, 8));
+    }
+    body.setTexture(&texture);
+    body.move(sf::Vector2f(gameManager->tileWidth * gridPos.x, gameManager->tileHeight * gridPos.y));
 }
 
 void Snack::Draw(sf::RenderWindow& rw)
 {
-    if(snackType == SmallSnack)
-        rw.draw(body);
+    rw.draw(body);
 }
