9f5a057d1e92fa62d7a8a2b0ea958ac32e006bd0
NicolasPCouts <nicolaspereiracoutinho@gmail.com>
2020-06-04 23:48:08 -0300

    improving debugging API

--- Src/Debugger/Debug.cpp
+++ Src/Debugger/Debug.cpp
@@ -60,6 +60,35 @@ void DrawGrid(sf::RenderWindow& window)
     }
 }
 
+void DrawCube(sf::RenderWindow& window, sf::Vector2i pos)
+{
+    sf::Vector2f vec;
+    vec.x = pos.x * gameManager->tileWidth;
+    vec.y = pos.y * gameManager->tileHeight;
+    sf::VertexArray quad(sf::LinesStrip, 5);
+
+    quad[0].color = sf::Color::Red;
+    quad[1].color = sf::Color::Red;
+    quad[2].color = sf::Color::Red;
+    quad[3].color = sf::Color::Red;
+    quad[4].color = sf::Color::Red;
+
+    quad[0].position = vec;
+    sf::Vector2f v = vec;
+    v.x += gameManager->tileWidth;
+    quad[1].position = v;
+
+    quad[2].position = sf::Vector2f(vec.x + gameManager->tileWidth, vec.y + gameManager->tileHeight);
+
+    sf::Vector2f v2 = vec;
+    v2.y += gameManager->tileHeight;
+    quad[3].position = v2;
+
+    quad[4].position = vec;
+
+    window.draw(quad);
+}
+
 void DrawPathfinding(sf::RenderWindow& window, std::vector<sf::Vector2i>& path, sf::Vector2i pathStart, sf::Vector2i pathTarget)
 {
     for (sf::Vector2i pos : path)
--- Src/Debugger/Debug.h
+++ Src/Debugger/Debug.h
@@ -2,4 +2,5 @@
 #include "../Common.h"
 
 void DrawGrid(sf::RenderWindow& window);
+void DrawCube(sf::RenderWindow& window, sf::Vector2i pos);
 void DrawPathfinding(sf::RenderWindow& window, std::vector<sf::Vector2i>& path, sf::Vector2i pathStart, sf::Vector2i pathTarget);
