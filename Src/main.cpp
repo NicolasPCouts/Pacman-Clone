f86ef5eefb2080ff3a26963a17b1edeb4afa3c1e
NicolasPCouts <nicolaspereiracoutinho@gmail.com>
2021-04-04 23:20:23 -0300

    removing console window from release version

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,8 +1,14 @@
 #include "GameManager.h"
+#include <Windows.h>
 GameManager* gameManager = new GameManager();
 
 int main()
 {
+#ifdef NDEBUG
+    HWND hwnd = GetConsoleWindow();
+    ShowWindow(hwnd, SW_HIDE);
+#endif
+
     gameManager->StartGameManager();
 
     gameManager->Update();
